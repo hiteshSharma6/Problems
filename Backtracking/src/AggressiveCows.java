07bb3dcd153bce16886c38b4e5271ce917e760dc
Hitesh Sharma <hitesh6sharma@gmail.com>
2017-12-16 23:39:00 +0530

    init

--- Backtracking/src/AggressiveCows.java
+++ Backtracking/src/AggressiveCows.java
@@ -1,17 +1,55 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class AggressiveCows {
+	static long stallNo[];
+	static long maxDistance = 0;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the array size: ");
+		System.out.println("Enter the number of stalls used: ");
 		int n = sc.nextInt();
-		int[] arr = new int[n];
-		for(int i = 0; i < n; ++i) {
-			arr[i] = sc.nextInt();
+		stallNo = new long[n];
+		System.out.println("Enter the stall no's in array: ");
+		for ( int i = 0; i < n ; i++ ){
+            stallNo[i] = sc.nextInt();
 		}
-		countInversions(arr);
+		System.out.println("Enter the number of cows: ");
+		int c = sc.nextInt();
+		Arrays.sort(stallNo);
+		minCowSpace(0, stallNo[n-1]-stallNo[0], c);
+		System.out.println("Maximum distance is: "+ maxDistance);
 
+	}
+
+	private static void minCowSpace(long start, long end, int cows) {
+		if(start > end) {
+			return;
+		}
+		long space = (end + start)/2;
+		System.out.println(space+"" + start+"" + end);
+		if(possible(0, stallNo.length, cows-1, space)) {
+			maxDistance = space;
+			minCowSpace(space+1, end, cows);
+		}
+		else {
+			minCowSpace(start, space-1, cows);
+		}
+	}
+
+	private static boolean possible(int start, int end, int cows, long space) {
+		if(cows == 0) {
+			return true;
+		}
+		if(start >= end) {
+			return false;
+		}
+		for(int i=start+1; i<end; ++i) {
+			if(stallNo[i] - stallNo[start] >= space) {
+				return possible(i, end, cows-1, space);
+			}
+		}
+		return false;
 	}
 
 }
--- Backtracking/src/CountInversion.java
+++ Backtracking/src/CountInversion.java
@@ -1,9 +1,60 @@
+import java.util.Scanner;
 
 public class CountInversion {
+	
+	private static int divide(int arr[], int start, int end) {
+		if(start >= end) {
+			return 0;
+		}
+		int mid = (start + end)/2;
+		int a = divide(arr, start, mid);
+		int b = divide(arr, mid + 1, end);
+		int c = merge(arr, start, mid, end);
+		return a+b+c;
+	}
+
+	private static int merge(int[] arr, int start, int mid, int end) {
+		int count = 0, i = start, j = mid+1, k = 0;
+		int sorted[] = new int[end - start + 1];
+		while(i<=mid && j<=end) {
+			if(arr[j] < arr[i]) {	//inversion - second array has element of less value
+				sorted[k++] = arr[j++];
+				count += mid - i + 1;
+			}
+			else {
+				sorted[k++] = arr[i++];
+			}
+		}
+		while(i <= mid) {
+			sorted[k++] = arr[i++];
+		}
+		while(j <= end) {
+			sorted[k++] = arr[j++];
+		}
+		System.arraycopy(sorted, 0, arr, start, end-start+1);
+		return count;
+	}
+	
+	static void printArray(int A[], int size)
+	{
+	    int i;
+	    for (i=0; i < size; i++)
+	        System.out.print(A[i]);
+	    System.out.println();
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of elements: ");
+		int n = sc.nextInt();
+		System.out.println("Enter the array: ");
+		int arr[] = new int[n];
+		for(int i=0; i < n; ++i)
+			arr[i] = sc.nextInt();
+		int count = divide(arr, 0, n-1);
+		System.out.println("Sorted array");
+		printArray(arr, n);
+		System.out.println("Number of inversions(A[i]>A[j], i<j): "+ count);
 	}
 
 }
